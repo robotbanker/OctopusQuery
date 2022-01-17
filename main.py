99f85ca744dc01701037545838c4cc70f312b2e9
robotbanker <davidesolla@outlook.it>
2022-01-17 16:57:08 +0000

    added monthly summarization methods under rolling consumptions

--- main.py
+++ main.py
@@ -22,9 +22,11 @@ class OctopusConsumption:
         self.halfhrly_consumption = self.fetch_energ_consumption(group_by='', page_size=25000)
 
     def fetch_energ_consumption(self, group_by='', page_size=100):
+        # sending request to API
         endpoint = f"electricity-meter-points/1200050242456/meters/21L4369794/consumption/?page_size={page_size}&group_by={group_by}"
         link = self.API_entry_point + endpoint
         r = requests.get(link, auth=(API_key, "")).json()
+        # cast and clean data
         consumptions = pd.json_normalize(r["results"])
         consumptions['price'] = consumptions['consumption'] * self.pricekwh
         consumptions['standing charge'] = self.dailychrg[group_by]
@@ -34,6 +36,9 @@ class OctopusConsumption:
                                           consumptions['interval_start']]
         consumptions['interval_end'] = [(datetime.strptime(timestamps, "%Y-%m-%dT%H:%M:%S%f%z")) for timestamps in
                                         consumptions['interval_end']]
+        consumptions['interval_start'] = pd.to_datetime(consumptions['interval_start'], utc=True)
+        consumptions['interval_end'] = pd.to_datetime(consumptions['interval_end'], utc=True)
+        # handle exception, if no data received print message to console:
         if consumptions.empty:
             print("-------------------------------------------------------")
             print("No data on energy consumptions available at this time")
@@ -49,23 +54,45 @@ class OctopusConsumption:
         print(f'Your life to date energy consumption is {ltd_consumption} kW/h')
 
     def rolling_consumption(self):
-        """return consumption as of yesterday and current month"""
-        consumption = self.halfhrly_consumption
+        """return consumption as of yesterday and current month and prior month"""
+        consumption = self.halfhrly_consumption  # use half hourly API
+        # from octopus timestamp create two columns with time and day
         consumption['string time'] = [x.strftime('%Y-%m-%d') for x in consumption['interval_start']]
         consumption['timestamp'] = [x.strftime('%H:%M') for x in consumption['interval_start']]
+        # find yesterday date
         yesterday = (datetime.today() - timedelta(1)).strftime('%Y-%m-%d')
-        yesterday_consumption = consumption.copy()[consumption['string time'] == yesterday]
-        busiest_hours = yesterday_consumption.nlargest(n=3, columns=['consumption','totalprice'])
-        print(busiest_hours)
+        # pull metrics for yesterday consumption
+        yesterday_consumption = consumption.copy()[consumption['string time'] == yesterday]  # pandas permutations are
+        # done on a copy of the original pandas
+        busiest_hours = yesterday_consumption.nlargest(n=3, columns=['consumption', 'totalprice'])
         hours = []
         [hours.append(x) for x in busiest_hours['timestamp']]
-        current_month =
+        # find current and prior months
+        current_month = datetime.today().strftime('%m')
+        current_year = datetime.today().strftime('%Y')
+        current_month_string = datetime.today().strftime('%B')
+        prior_month_date = datetime.today().replace(day=1) - timedelta(1)
+        prior_period_month = prior_month_date.strftime('%m')
+        prior_period_year = prior_month_date.strftime('%Y')
+        prior_month_string = prior_month_date.strftime('%B')
+        # pull metrics for current and prior months
+        current_month_data = consumption[(consumption['interval_start'].dt.month == int(current_month)) & (consumption['interval_start'].dt.year == int(current_year))]
+        prior_month_data = consumption[(consumption['interval_start'].dt.month == int(prior_period_month)) & (consumption['interval_start'].dt.year == int(prior_period_year))]
+
+        current_month_consumption = current_month_data['totalprice'].sum()
+        prior_month_consumption = prior_month_data['totalprice'].sum()
+        print(current_month_consumption)
+        print(prior_month_consumption)
 
 
-        moralizzatore = f"Yesterday {yesterday} you consumed a total of {round(yesterday_consumption['consumption'].sum(),2)} kWh." \
-                        f"this amount of energy cost you a total of £ {round(yesterday_consumption['totalprice'].sum(),2)}." \
-                        f"The busiest hours were {', '.join(hours)}, where the total charge was £ {round(busiest_hours['totalprice'].sum(),2)}. "
-        print (moralizzatore)
+
+        moralizzatore = f"Yesterday {yesterday} you consumed a total of " \
+                        f"{round(yesterday_consumption['consumption'].sum(), 2)} kWh, " \
+                        f"this amount of energy cost you £ " \
+                        f"{round(yesterday_consumption['totalprice'].sum(), 2)}.\n" \
+                        f"The busiest 30 minutes periods were the ones at: {', '.join(hours)}, where the total cost charged was £" \
+                        f" {round(busiest_hours['totalprice'].sum(), 2)}.\nFor the month of {current_month_string} you spent £ {round(current_month_consumption, 2)} so far, while in {prior_month_string} you spent a total of £ {round(prior_month_consumption, 2)}"
+        print(moralizzatore)
 
 
 run = OctopusConsumption()
