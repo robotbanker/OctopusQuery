594eae32057daff95b1b47e135deaae56552849b
robotbanker <davidesolla@outlook.it>
2022-01-17 13:38:53 +0000

    added conversion of datetime strings in datetime

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 import requests
 import pandas as pd
-from datetime import datetime
+from datetime import datetime, timedelta
 import matplotlib.pyplot as plt
 from secrets import API_key
 
 
-class Octopus:
-    dailycharge = 0.2330 # GBP VAT Inc - standing charge per each day.
+class OctopusConsumption:
+    dailycharge = 0.2330  # GBP VAT Inc - standing charge per each day.
     pricekwh = 0.2017  # GBP VAT Incl
     API_entry_point = "https://api.octopus.energy/v1/"
     dailychrg = {
@@ -18,22 +18,22 @@ class Octopus:
     }
 
     def __init__(self):
-        self.energy_consumption = self.fetch_energ_consumption()
+        self.daily_consumption = self.fetch_energ_consumption(group_by='day', page_size=25000)
+        self.halfhrly_consumption = self.fetch_energ_consumption(group_by='', page_size=25000)
 
-    def fetch_energ_consumption(self, group_by='day', page_size=1000):
+    def fetch_energ_consumption(self, group_by='', page_size=100):
         endpoint = f"electricity-meter-points/1200050242456/meters/21L4369794/consumption/?page_size={page_size}&group_by={group_by}"
         link = self.API_entry_point + endpoint
         r = requests.get(link, auth=(API_key, "")).json()
         consumptions = pd.json_normalize(r["results"])
-        consumptions['price'] = consumptions['consumption']*self.pricekwh
-        consumptions['standig charge'] = self.dailychrg[group_by]
-        consumptions['totalprice'] = consumptions['price']+consumptions['standig charge']
-        #parse date time strings as datetime
+        consumptions['price'] = consumptions['consumption'] * self.pricekwh
+        consumptions['standing charge'] = self.dailychrg[group_by]
+        consumptions['totalprice'] = consumptions['price'] + consumptions['standing charge']
+        # parse date time strings as datetime
         consumptions['interval_start'] = [(datetime.strptime(timestamps, "%Y-%m-%dT%H:%M:%S%f%z")) for timestamps in
-                                 consumptions['interval_start']]
+                                          consumptions['interval_start']]
         consumptions['interval_end'] = [(datetime.strptime(timestamps, "%Y-%m-%dT%H:%M:%S%f%z")) for timestamps in
-                                 consumptions['interval_end']]
-
+                                        consumptions['interval_end']]
         if consumptions.empty:
             print("-------------------------------------------------------")
             print("No data on energy consumptions available at this time")
@@ -41,6 +41,30 @@ class Octopus:
             print("-------------------------------------------------------")
         else:
             return consumptions
+
+    def meter_reading(self):
+        """this method returns the LTD energy consumption by first querying last 25,000 days from the API."""
+        consumptions = self.daily_consumption
+        ltd_consumption = consumptions['consumption'].sum()
+        print(f'Your life to date energy consumption is {ltd_consumption} kW/h')
+
+    def rolling_consumption(self):
+        """return consumption as of yesterday and current month"""
+        consumption = self.halfhrly_consumption.copy()
+        consumption['string time'] = [x.strftime('%Y-%m-%d') for x in consumption['interval_start']]
+        yesterday = (datetime.today() - timedelta(1)).strftime('%Y-%m-%d')
+        yesterday_consumption = consumption[consumption['string time'] == yesterday]
+
+
+run = OctopusConsumption()
+# run.plot_tariff()
+run.rolling_consumption()
+
+
+class OctopusTariffs:
+    dailycharge = 0.2330  # GBP VAT Inc - standing charge per each day.
+    pricekwh = 0.2017  # GBP VAT Incl
+    API_entry_point = "https://api.octopus.energy/v1/"
 
     def fetch_tariff(self):
         tariffs_endpoint = "products/AGILE-18-02-21/electricity-tariffs/E-1R-AGILE-18-02-21-C/standard-unit-rates/"
@@ -64,13 +88,3 @@ class Octopus:
         mng = plt.get_current_fig_manager()
         mng.full_screen_toggle()
         plt.show()
-
-    def meter_reading(self):
-        """this method returns the LTD energy consumption by first querying last 25,000 days from the API."""
-        consumptions = self.energy_consumption
-        ltd_consumption = consumptions['consumption'].sum()
-        print(f'Your life to date energy consumption is {ltd_consumption} kW/h')
-
-run = Octopus()
-# run.plot_tariff()
-print(run.fetch_energ_consumption())
